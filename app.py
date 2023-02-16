45b82cfe0c7de62afaddd95964b9ac8a6a5dbed1
Крупий Владимир <getcher123@gmail.com>
2023-02-16 14:56:26 +0300

    first commit

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 import asyncio
 import os
 
+from aiogram import executor
+
+from loader import dp
 from utils import set_default_commands, check_new_houses
 import handlers
 
-import logging
-from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-from settings import (BOT_TOKEN, HEROKU_APP_NAME,
-                          WEBHOOK_URL, WEBHOOK_PATH,
-                          WEBAPP_HOST, WEBAPP_PORT)
-
-
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
-
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
 
 
 async def on_startup(dispatcher):
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
     loop = asyncio.get_event_loop()
-    loop.create_task(check_new_houses(dp, 60))
+    loop.create_task(check_new_houses(60))
 
 
 if __name__ == '__main__':
--- utils/check_houses.py
+++ utils/check_houses.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from loader import bot
 from messages import MESSAGES
 from home_parser import MyHomeParser
 
@@ -8,7 +9,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-async def check_new_houses(dp, sleep_time: int):
+async def check_new_houses(sleep_time: int):
     while True:
         await asyncio.sleep(sleep_time)
         url = os.environ.get('URL')
@@ -30,10 +31,10 @@ async def check_new_houses(dp, sleep_time: int):
         urls_str = '\n'.join(p.homes_url)
         msg = f"{MESSAGES['house_is_found']}\n\n{urls_str}"
 
-        user_ids = os.environ.get('USER_IDS').split(',')
+        user_ids = os.environ.get('USER_IDS', '').split(',')
         logging.info('user_ids = ', user_ids)
         for user_id in user_ids:
             try:
-                await dp.bot.send_message(user_id, msg)
+                await bot.send_message(user_id, msg)
             except Exception as e:
                 print(e)

504cee3f50f3d8908bd7f7e7032f32fdecc8a834
Крупий Владимир <getcher123@gmail.com>
2023-02-16 14:33:05 +0300

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import asyncio
 import os
 
-from aiogram import executor
-
-from loader import dp
 from utils import set_default_commands, check_new_houses
-import handlers
+import handler
+
+import logging
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from settings import (BOT_TOKEN, HEROKU_APP_NAME,
+                          WEBHOOK_URL, WEBHOOK_PATH,
+                          WEBAPP_HOST, WEBAPP_PORT)
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
 async def on_startup(dispatcher):
@@ -18,3 +32,5 @@ async def on_startup(dispatcher):
 if __name__ == '__main__':
 
     executor.start_polling(dp, on_startup=on_startup)
+
+
